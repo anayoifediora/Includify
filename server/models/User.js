aba504b80ff32c407504d15e28a93f2871eec13b
Kanayo <anayoifediora@live.com>
2025-01-21 09:52:02 +1030

    completed user model

--- server/models/User.js
+++ server/models/User.js
@@ -30,9 +30,43 @@ const userSchema = new Schema({
     sex: {
         type: String,
         enum: ['Male', 'Female'],
+    },
+    address: {
+        type: String,
+        required: true,
+    },
+    supervisor: {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+    },
+    clients: [{
+        type: Schema.Types.ObjectId,
+        ref: 'client',
+    }],
+    locations: [location],
+
+},
+{
+    toJSON: {
+        virtuals: true,
+    },
+    id: false,
+});
+
+//Set up pre-save middleware to hash the password before it's created
+userSchema.pre('save', async function(next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds)
     }
 
-});
+    next();
+})
+
+//Compare incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function(password) {
+    return bcrypt.compare(password, this.password);
+}
 
 const User = model('User', userSchema);
 
